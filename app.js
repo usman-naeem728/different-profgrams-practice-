fe7cfb8d3d56917720888fdce2c15714cdb878c4
Muhammad Usman Naeem <76055167+usman-naeem728@users.noreply.github.com>
2022-06-13 12:23:57 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+// question 1 
+
+// for (let index = 0; index < 6; index++) {
+//     document.write("hello world" + "</br>");
+    
+// }
+
+
+//  question 2
+
+// for(i = 0; i <11; i++ ){
+//     document.write(i + "</br>")
+// }
+
+// question 3
+
+// let inp = prompt("enter a number");
+// let num = Number(inp);
+// let length = prompt("length of table");
+// let length1 = Number(length);
+
+// for(i = 0; i <= length1; i++ ){
+//     document.write(`${num} X ${i}  = ${num * i} </br>`)
+// }
+
+// question 4 
+
+// let a = ["nokia","samsung","apple","sony","huawei"]
+
+// for(i=0; i < a.length; i++){
+//     document.write(a[i]+"</br>")
+// }
+
+// question 5
+
+// let a = ["nokia","samsung","apple","sony","huawei"]
+
+// for(i=0; i < a.length; i++){
+//     document.write(a[i]+"</br>")
+// }
+// for(i= 0 ; i< a.length; i++){
+//     document.write(" elment at  index " + i + " is "  + a[i] +"</br>" )
+// } 
+
+// question 6
+// let inp = prompt("enter number of items");
+// let length = Number(inp);
+// var sub_array = [];
+
+//     for (var i = 0; i < length; i++) {
+//     var item = prompt("enter " + i + "item");
+//         sub_array.push(item);
+//     }
+// document.write(sub_array);
